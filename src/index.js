0da8d98d185fc93bb3160b32167647bdbb211a3a
TahaRizwan <tahabhaiyag@gmail.com>
2022-04-24 21:32:15 +0500

    init

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App  name="Taha Rizwan" age={25}/>
+    <h1 className="blue">Yes</h1>
   </React.StrictMode>
 );
 
@@ -15,3 +16,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
